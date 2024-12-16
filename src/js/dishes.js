c0c7b5297deb8d308a38958d1d125e67f5746c4b
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-12-16 22:56:16 +0200

    test

---
+++ src/js/dishes.js
@@ -0,0 +1,22 @@
+export const products = [
+    {
+        name: 'apple',
+        price: 100
+    },
+    {
+        name: 'kivi',
+        price: 430
+    },
+    {
+        name: 'grape',
+        price: 123
+    },
+    {
+        name: 'mango',
+        price: 198
+    },
+    {
+        name: 'ananas',
+        price: 302
+    },
+]
